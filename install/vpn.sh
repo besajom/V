8d152fe03cb673be9f6c4f2b836d3b1c17d7b1d1
besajom <bimag9772@gmail.com>
2024-06-01 16:06:08 +0800

    Update vpn.sh

--- install/vpn.sh
+++ install/vpn.sh
@@ -4,7 +4,7 @@
 # By RMBL
 # ==================================================
 # Link Hosting Kalian
-RMBL="raw.githubusercontent.com/VIP-VPN/vip/main/install/"
+RMBL="raw.githubusercontent.com/besajom/vip/main/install/"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
@@ -18,7 +18,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/VIP-VPN/vip/main/install/vpn.zip
+wget https://raw.githubusercontent.com/besajom/vip/main/install/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
